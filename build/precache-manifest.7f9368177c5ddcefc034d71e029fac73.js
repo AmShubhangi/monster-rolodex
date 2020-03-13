ff0b957f0d9228082b3c400d77303ff4d7be72a8
Shubhangi <shubhambade6897@gmail.com>
2020-03-13 18:40:00 +0530

    build folder

---
+++ build/precache-manifest.7f9368177c5ddcefc034d71e029fac73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7892c425ee6e5f2e1c70a7c214bbc44",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "0a5fe158d0b96775f789",
+    "url": "/monsters/static/css/main.3b7dd1de.chunk.css"
+  },
+  {
+    "revision": "03961aefbff84ff13ab7",
+    "url": "/monsters/static/js/2.923885a9.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/monsters/static/js/2.923885a9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a5fe158d0b96775f789",
+    "url": "/monsters/static/js/main.6c6920ec.chunk.js"
+  },
+  {
+    "revision": "8bdf229af4cb29d7dd8a",
+    "url": "/monsters/static/js/runtime-main.bca33ad4.js"
+  }
+]);
